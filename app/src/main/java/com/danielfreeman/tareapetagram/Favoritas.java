55ba9c72e5a22b0bb9487d76763ed48d7ddad42f
DanFreeman13 <dan_freeman@live.com.mx>
2019-06-10 18:11:55 -0500

    se agregó funcionalidad de boton atras a la segunda activity

--- app/src/main/java/com/danielfreeman/tareapetagram/Favoritas.java
+++ app/src/main/java/com/danielfreeman/tareapetagram/Favoritas.java
@@ -2,6 +2,7 @@ package com.danielfreeman.tareapetagram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class Favoritas extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class Favoritas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favoritas);
+
+        Toolbar miActionBarNoStar = (Toolbar) findViewById(R.id.miActionBar_noStar);
+        setSupportActionBar(miActionBarNoStar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
